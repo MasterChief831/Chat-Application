6e4042e65d38ed5e0060fd40492af4f15966aa2b
MasterChief831 <masterchieff831831@gmail.com>
2024-05-31 15:18:45 +0530

    Chat and LogIn Componenet Created

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
